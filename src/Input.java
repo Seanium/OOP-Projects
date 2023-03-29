5a969496ee78769915a88b6336639df04adcf59c
sean <2579935313@qq.com>
2023-03-29 11:55:23 +0800

    feat: 完成需求：支持电梯的动态增加、动态停运

---
+++ src/Input.java
@@ -0,0 +1,52 @@
+import com.oocourse.elevator2.ElevatorInput;
+import com.oocourse.elevator2.ElevatorRequest;
+import com.oocourse.elevator2.MaintainRequest;
+import com.oocourse.elevator2.PersonRequest;
+import com.oocourse.elevator2.Request;
+
+import java.io.IOException;
+
+public class Input extends Thread {
+    private final Table table;
+
+    public Input(Table table) {
+        this.table = table;
+    }
+
+    @Override
+    public void run() {
+        ElevatorInput elevatorInput = new ElevatorInput(System.in);
+        while (true) {
+            Request request = elevatorInput.nextRequest();
+            // when request == null
+            // it means there are no more lines in stdin
+            if (request == null) {
+                table.setEnd(true);
+                break;
+            } else {
+                // a new valid request
+                if (request instanceof PersonRequest) {
+                    // a PersonRequest
+                    // your code here
+                    table.put((PersonRequest) request);
+                } else if (request instanceof ElevatorRequest) {
+                    // an ElevatorRequest
+                    // your code here
+                    new Elevator(table, ((ElevatorRequest) request).getElevatorId(),
+                            ((ElevatorRequest) request).getFloor(),
+                            ((ElevatorRequest) request).getCapacity(),
+                            ((ElevatorRequest) request).getSpeed()).start();
+                } else if (request instanceof MaintainRequest) {
+                    // an MaintainRequest
+                    // your code here
+                    table.setMaintainable(((MaintainRequest) request).getElevatorId(), true);
+                }
+            }
+        }
+        try {
+            elevatorInput.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import com.oocourse.elevator2.TimableOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        TimableOutput.initStartTimestamp();  // 初始化时间戳
+        Table table = new Table(1, 11);
+        new Input(table).start();
+        for (int i = 1; i <= 6; i++) {
+            new Elevator(table, i, 1, 6, 0.4).start();
+        }
+    }
+}
